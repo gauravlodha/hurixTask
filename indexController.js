41061c09cef8d4da9312703af02d6b2288ab6211
Gaurav Lodha <gaurav.lodha@hsmumsz.local>
2017-10-12 11:25:43 +0530

    Angular.parseJSON 12oct

--- indexController.js
+++ indexController.js
@@ -1,13 +1,28 @@
-var app = angular.module("bookStore", []);
-
-
-app.controller('indexController', function myFunction ($scope, test) {
-    {
-    	// $scope.data = {};
-    	test.getData().then(function(data){
-    	 $scope.bookList = data.data;
+var app = angular
+            .module("bookStore", [])
+            .controller('indexController', function myFunction($scope, $http) {
+    $http.get('bookService.json')
+    .then(function(result) {
+        //console.log(result.data);
+        $scope.bookList = angular.fromJson(result.data);
+        //console.log($scope.bookList);
     });
-}
+$scope.addToCart = function (item) {
+        alert('Item added into cart');
+        alert(item.bookName +" | "+ item.price +" | "+ item.author);
+    };
+});
+
+
+/*app.controller('indexController', function myFunction ($scope, test) {
+   {
+    	 $scope.data = {};
+     	test.getData().then(function(data){
+     	 $scope.bookList = data.data;
+    });
+ }
+
+
      $scope.addToCart = function (item) {
         alert('Item added into cart');
         alert(item.bookName +" | "+ item.price +" | "+ item.author);
@@ -18,4 +33,6 @@ app.service('test', function ($http) {
     this.getData = function () {
         return $http.get('bookService.json');
     }
-});
+});*/
+
+
